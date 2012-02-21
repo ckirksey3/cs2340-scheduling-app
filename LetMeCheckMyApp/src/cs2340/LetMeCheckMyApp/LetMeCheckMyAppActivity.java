432928f3cd00ec62c81f0abf834b5afcca44f53c
ckirksey3@gmail.com <ckirksey3@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-02-21 12:44:03 +0000

    Added new registration page, register activity, and functionality for RegisterUserButton

---
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/LetMeCheckMyAppActivity.java
@@ -0,0 +1,26 @@
+package cs2340.LetMeCheckMyApp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LetMeCheckMyAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button registerNewUser = (Button)findViewById(R.id.registerNewButton);
+        registerNewUser.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				// TODO Auto-generated method stub
+				Intent myIntent = new Intent(view.getContext(), RegisterUser.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
+    }
+}
