364e79e03fa90d93bae120b848bd0e40c5083e66
ckirksey3@gmail.com <ckirksey3@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-02-22 19:40:29 +0000

    Fixed compile errors

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
@@ -11,6 +11,6 @@ public class ManageTaskList extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration);
+        setContentView(R.layout.tasklist);
     }
 }
