1dd839e89b3ced4466b85c916d368f94d35a73a0
jpansini3 <jpansini3@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-02-23 20:16:58 +0000

    javadoc

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/RegisterUser.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/RegisterUser.java
@@ -6,6 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * 
+ * The activity class for 
+ * the new user registration screen
+ * 
+ * 
+ * @author Jeff
+ *
+ */
 public class RegisterUser extends Activity {
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,10 @@ public class RegisterUser extends Activity {
 
 	    Button next = (Button) findViewById(R.id.button1);
 	    next.setOnClickListener(new View.OnClickListener() {
+	    	/**
+	    	 * when the registration button is clicked, this is executed
+	    	 * and the screen will change.
+	    	 */
 	        public void onClick(View view) {
 	            Intent intent = new Intent();
 	            setResult(RESULT_OK, intent);
