6c74e3a316c7023a730e8a502aa7d262a2b3b1d1
jpansini3 <jpansini3@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-02-23 20:32:51 +0000

    javadoc

--- LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/RegisterUser.java
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/RegisterUser.java
@@ -23,10 +23,7 @@ public class RegisterUser extends Activity {
 
 	    Button next = (Button) findViewById(R.id.button1);
 	    next.setOnClickListener(new View.OnClickListener() {
-	    	/**
-	    	 * when the registration button is clicked, this is executed
-	    	 * and the screen will change.
-	    	 */
+	    	/** When the registration button is clicked, this code is executed */
 	        public void onClick(View view) {
 				// TODO Auto-generated method stub
 				Intent myIntent = new Intent(view.getContext(), ManageTaskList.class);
