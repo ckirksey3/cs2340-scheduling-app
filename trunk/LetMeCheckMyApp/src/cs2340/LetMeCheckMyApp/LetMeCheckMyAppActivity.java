3502494725df8e180cc9ad121158560c7cffbcee
dandoozan <dandoozan@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-02-25 20:15:01 +0000

    When a user registers, the data is stored in a file called "users.dat" When a user logs in, the entered information is verified against the data in the file. Also, messages are displayed to indicate correct or incorrect logins. Slice 2 should be complete now.

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/LetMeCheckMyAppActivity.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/LetMeCheckMyAppActivity.java
@@ -1,40 +1,102 @@
 package cs2340.LetMeCheckMyApp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LetMeCheckMyAppActivity extends Activity {
-    /** Called when the activity is first created. */
+    private final String USER_FILE = "users.dat";
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final EditText userNameET = (EditText) findViewById(R.id.userNameText);
+        final EditText passwordET = (EditText) findViewById(R.id.passwordText);
+        
         Button registerNewUserButton = (Button)findViewById(R.id.registerNewButton);
         registerNewUserButton.setOnClickListener(new View.OnClickListener()
-        
         {
-			
 			public void onClick(View view) {
-				// TODO Auto-generated method stub
 				Intent myIntent = new Intent(view.getContext(), RegisterUser.class);
 				startActivityForResult(myIntent, 0);
 			}
 		});
         Button signInButton = (Button)findViewById(R.id.signInButton);
         signInButton.setOnClickListener(new View.OnClickListener() {
-			
-			/**
-			 * 
-			 */
 			public void onClick(View view) {
-				// TODO Auto-generated method stub
-				Intent myIntent = new Intent(view.getContext(), ManageTaskList.class);
-				startActivityForResult(myIntent, 0);
+				String username = userNameET.getText().toString();
+                String password = passwordET.getText().toString();
+				if(username.equals("") || password.equals("")){
+					displayMessage("Enter both a username and password");
+				}else if(verifyUser(username,password)){
+					Log.d("SignIn", "Signing in as \""+username+"\"");
+					displayMessage("SignIn Succesful!");
+					
+					Intent myIntent = new Intent(view.getContext(), ManageTaskList.class);
+                	startActivityForResult(myIntent, 0);
+				}else{
+					displayMessage("Username or password is incorrect");
+                }
 			}
 		});
     }
+    
+    /**
+     * Verify that the user info matches an existing user.
+     * @param username	Username entered by user
+     * @param password	Password entered by user
+     * @return	true if info matches, false otherwise
+     */
+    private boolean verifyUser(String username, String password){
+    	Log.d("SignIn", "Verfying user credentials for \""+username+"\"");
+    	boolean found = false;
+		BufferedReader br = null;
+		try{
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(getFilesDir()+"/"+USER_FILE))));
+			String line;
+			while((line = br.readLine()) != null && !found){
+				String[] items = line.split("\t");
+				if(items[0].equals(username) && items[1].equals(password)){
+					found = true;
+				}
+				//Log.d("tbd", "Line="+line+", username="+items[0]+", password="+items[1]+", email="+items[2]+", found="+found);
+			}
+		}catch(IOException e){
+			Log.e("SignIn", e.getMessage());
+		}finally {
+			try{
+				if(br != null)
+					br.close();
+			}catch(IOException e){
+				Log.e("SignIn", e.getMessage());
+			}
+		}
+		return found;
+    }
+    
+    /**
+	 * Displays an error message to the user
+	 * @param msg	Message to be displayed
+	 */
+	private void displayMessage(String msg){
+		Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, msg, duration);
+        toast.show();
+	}
 }
