87c49197035e878e5690573afe0415c7df15cc1a
samuel.bell.jr@gmail.com <samuel.bell.jr@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-02-25 18:57:53 +0000

    Removed some superfluous imports in ManageTaskList.java.

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
@@ -1,10 +1,7 @@
 package cs2340.LetMeCheckMyApp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class ManageTaskList extends Activity {
     /** Called when the activity is first created. */
