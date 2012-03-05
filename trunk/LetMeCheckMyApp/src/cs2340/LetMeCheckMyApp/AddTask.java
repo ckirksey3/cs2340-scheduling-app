c1a4eacf804a6ab1265514d03600e0e48d58558b
ckirksey3@gmail.com <ckirksey3@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-03-05 18:27:17 +0000

    Added AddTask activity and view

---
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
@@ -0,0 +1,13 @@
+package cs2340.LetMeCheckMyApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddTask extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addtask);
+    }
+}
