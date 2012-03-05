7de409d15862a3114a2f2a6b0ce001d4c6e3e814
ckirksey3@gmail.com <ckirksey3@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-03-05 20:35:12 +0000

    Updated tasklist.xml to include ListView of tasks and added DeleteTask activity

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class ManageTaskList extends Activity {
     /** Called when the activity is first created. */
@@ -12,6 +14,12 @@ public class ManageTaskList extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasklist);
+        
+        String[] currentTaskList = new String[]{"Buy groceries", "Take out trash", "Take over the world"};
+        
+        ListView listview = (ListView) findViewById(R.id.TaskList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, currentTaskList);
+        listview.setAdapter(adapter);
         
         Button addTaskButton = (Button)findViewById(R.id.AddTaskButton);
         addTaskButton.setOnClickListener(new View.OnClickListener()
