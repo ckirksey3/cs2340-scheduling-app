c4843c54d85e1e74bac6047e8ce205aaa80f771a
ckirksey3@gmail.com <ckirksey3@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-03-05 20:35:12 +0000

    Updated tasklist.xml to include ListView of tasks and added DeleteTask activity

--- LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
@@ -1,7 +1,11 @@
 package cs2340.LetMeCheckMyApp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class AddTask extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,21 @@ public class AddTask extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addtask);
+        
+        Button addTaskButton = (Button)findViewById(R.id.CompleteAddTaskButton);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+        	/** 
+        	 * When the sign in button is clicked, this code is executed 
+        	 * */
+			public void onClick(View view) {
+				
+				/*
+				 * Add the task to the user's task list
+				 *  */
+				
+				Intent myIntent = new Intent(view.getContext(), ManageTaskList.class);
+            	startActivityForResult(myIntent, 0);
+			}
+		});
     }
 }
---
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/DeleteTask.java
@@ -0,0 +1,21 @@
+package cs2340.LetMeCheckMyApp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class DeleteTask extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        //what to pass for context?
+        Intent myIntent = new Intent(null, ManageTaskList.class);
+    	startActivityForResult(myIntent, 0);
+    }
+}
