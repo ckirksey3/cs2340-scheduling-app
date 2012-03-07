0a89e8499a138600ad995f8aa7b6c83308b2d91d
samuel.bell.jr@gmail.com <samuel.bell.jr@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-03-07 03:30:45 +0000

    Enabled local storage of an ArrayList of strings (the "todo list").

--- LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddTask extends Activity {
     /** Called when the activity is first created. */
@@ -14,19 +15,24 @@ public class AddTask extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addtask);
         
+        final EditText taskNameET = (EditText) findViewById(R.id.TaskNameText);
+        
         Button addTaskButton = (Button)findViewById(R.id.CompleteAddTaskButton);
         addTaskButton.setOnClickListener(new View.OnClickListener() {
         	/** 
         	 * When the sign in button is clicked, this code is executed 
-        	 * */
+        	 */
 			public void onClick(View view) {
 				
 				/*
-				 * Add the task to the user's task list
-				 *  */
+				 * Return the added task to the calling activity
+				 */
+				String taskName = taskNameET.getText().toString();
+				if (taskName != "") {
+	            	setResult(RESULT_OK, new Intent().putExtra("AddTask", taskName));
+	            	finish();
+				}
 				
-				Intent myIntent = new Intent(view.getContext(), ManageTaskList.class);
-            	startActivityForResult(myIntent, 0);
 			}
 		});
     }
