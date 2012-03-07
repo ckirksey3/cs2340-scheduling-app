6094f81a75137af40419e96837f01be63f1da643
samuel.bell.jr@gmail.com <samuel.bell.jr@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-03-07 04:07:18 +0000

    Removed some unused imports to get rid of little warnings in eclipse.

--- LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
@@ -3,7 +3,6 @@ package cs2340.LetMeCheckMyApp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
--- LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/DeleteTask.java
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/DeleteTask.java
@@ -3,9 +3,6 @@ package cs2340.LetMeCheckMyApp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 public class DeleteTask extends Activity {
     /** Called when the activity is first created. */
