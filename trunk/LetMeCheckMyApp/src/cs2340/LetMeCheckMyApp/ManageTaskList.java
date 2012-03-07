bbc39e6210f19efcd904c4b19a8cafac12c30eaf
samuel.bell.jr@gmail.com <samuel.bell.jr@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-03-07 03:30:45 +0000

    Enabled local storage of an ArrayList of strings (the "todo list").

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskList.java
@@ -1,5 +1,7 @@
 package cs2340.LetMeCheckMyApp;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,28 +11,47 @@ import android.widget.Button;
 import android.widget.ListView;
 
 public class ManageTaskList extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tasklist);
-        
-        String[] currentTaskList = new String[]{"Buy groceries", "Take out trash", "Take over the world"};
-        
-        ListView listview = (ListView) findViewById(R.id.TaskList);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, currentTaskList);
-        listview.setAdapter(adapter);
-        
-        Button addTaskButton = (Button)findViewById(R.id.AddTaskButton);
-        addTaskButton.setOnClickListener(new View.OnClickListener()
-        {
-        	/** 
-        	 * When the add new task button is clicked, this code is executed 
-        	 * */
+	
+	ArrayList<String> list;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tasklist);
+
+		ListView listview = (ListView) findViewById(R.id.TaskList);
+		//TODO attempt to load the user's list from storage
+		list = new ArrayList<String>();
+		final ArrayAdapter<String> adapter;
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+		listview.setAdapter(adapter);
+
+		Button addTaskButton = (Button)findViewById(R.id.AddTaskButton);
+		addTaskButton.setOnClickListener(new View.OnClickListener()
+		{
+			/** 
+			 * When the add new task button is clicked, this code is executed 
+			 * */
 			public void onClick(View view) {
 				Intent myIntent = new Intent(view.getContext(), AddTask.class);
 				startActivityForResult(myIntent, 0);
 			}
 		});
-    }
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK) {
+			list.add(data.getStringExtra("AddTask"));
+		}
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		//TODO save list to SD
+	}
 }
