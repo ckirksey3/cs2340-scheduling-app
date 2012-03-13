2056833801f2092f27715ed6cf3aa5ae30c3a43f
samuel.bell.jr@gmail.com <samuel.bell.jr@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-03-13 20:12:50 +0000

    Added the Calendar widget and changed the task's date storage to a Calendar object.

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
@@ -1,6 +1,6 @@
 package cs2340.LetMeCheckMyApp;
 
-import java.util.Date;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.content.Context;
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,22 +21,22 @@ import android.widget.Toast;
  *
  */
 public class AddTask extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.addtask);
-        
-        final EditText taskNameET = (EditText) findViewById(R.id.TaskNameText);
-        final EditText descriptionET = (EditText) findViewById(R.id.TaskDescriptionText);
-        final EditText categoryET = (EditText) findViewById(R.id.TaskCategoryText);
-        final EditText dateET = (EditText) findViewById(R.id.TaskDateText);
-        
-        Button addTaskButton = (Button)findViewById(R.id.CompleteAddTaskButton);
-        addTaskButton.setOnClickListener(new View.OnClickListener() {
-        	/** 
-        	 * When the Add Task button is clicked, this code is executed 
-        	 */
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addtask);
+
+		final EditText taskNameET = (EditText) findViewById(R.id.TaskNameText);
+		final EditText descriptionET = (EditText) findViewById(R.id.TaskDescriptionText);
+		final EditText categoryET = (EditText) findViewById(R.id.TaskCategoryText);
+		final DatePicker dateWidget = (DatePicker) findViewById(R.id.datePicker1);
+
+		Button addTaskButton = (Button)findViewById(R.id.CompleteAddTaskButton);
+		addTaskButton.setOnClickListener(new View.OnClickListener() {
+			/** 
+			 * When the Add Task button is clicked, this code is executed 
+			 */
 			public void onClick(View view) {
 				/*
 				 * Return the added task to the calling activity
@@ -43,43 +44,37 @@ public class AddTask extends Activity {
 				String taskName = taskNameET.getText().toString();
 				String description = descriptionET.getText().toString();
 				String category = categoryET.getText().toString();
-				String dateString = dateET.getText().toString();
 				
-				try
-				{
-					Date date = new Date(dateString);
-					if (taskName != "") {
-		            	setResult(RESULT_OK, new Intent().putExtra("AddTask", new Task(taskName,description, category, date)));
-		            	finish();
-					}
-				}
-				catch(Exception ex)
-				{
-					displayMessage("Improper date format");
-				}
+				Calendar date = Calendar.getInstance();
+				date.set(Calendar.YEAR, dateWidget.getYear());
+				date.set(Calendar.MONTH, dateWidget.getMonth());
+				date.set(Calendar.DAY_OF_MONTH, dateWidget.getDayOfMonth());
 				
+				setResult(RESULT_OK, new Intent().putExtra("AddTask", new Task(taskName,description, category, date)));
+				finish();
+
 			}
 		});
-        
-        // adds cancel button functionality
-        Button cancelButton = (Button)findViewById(R.id.CancelAddTaskButton);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
+
+		// adds cancel button functionality
+		Button cancelButton = (Button)findViewById(R.id.CancelAddTaskButton);
+		cancelButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				setResult(RESULT_CANCELED);
 				finish();
 			}
 		});
-        
-        
-    }
-    /**
+
+
+	}
+	/**
 	 * Displays an error message to the user
 	 * @param msg	Message to be displayed
 	 */
 	private void displayMessage(String msg){
 		Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, msg, duration);
-        toast.show();
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, msg, duration);
+		toast.show();
 	}
 }
