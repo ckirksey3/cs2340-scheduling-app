c32fa57c8f861c64a5aabf137e6352505abef597
jpansini3 <jpansini3@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-04-05 04:20:37 +0000

    Complete filter JUints

--- LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/FilterTest.java
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/FilterTest.java
@@ -10,7 +10,8 @@ public class FilterTest extends ActivityInstrumentationTestCase2<LetMeCheckMyApp
 	private LetMeCheckMyAppActivity mActivity;
 	private TextView mView;
 	private String resourceString;
-
+	private SpinnerListener sl;
+	private ArrayList<Task> list;
 
 	public FilterTest() {
 		super(LetMeCheckMyAppActivity.class);
@@ -23,53 +24,47 @@ public class FilterTest extends ActivityInstrumentationTestCase2<LetMeCheckMyApp
 		mActivity = this.getActivity();
 		//mView = (TextView) mActivity.findViewById(R.id.);
 		resourceString = mActivity.getString(R.string.hello);
+		ManageTaskList mtl = new ManageTaskList();
+		list = new ArrayList<Task>();
+		Task t0 = new Task();
+		Task t1 = new Task();
+		t0.setComplete(true);
+		t1.setComplete(false);
+		t0.setCategory("Work");
+		t1.setCategory("School");
+		list.add(t0);
+		list.add(t1);
+		mtl.setList(list);
+		sl = new SpinnerListener(mtl);
+
 	}
 
 	public void testCheckComplete() {
-		ArrayList<Task> list = new ArrayList<Task>();
-		Task t1 = new Task();
-		Task t2 = new Task();
-		t1.setComplete(true);
-		t2.setComplete(false);
-		list.add(t1);
-		list.add(t2);
-		assertEquals(t1.isVisible(), true);
-		assertEquals(t2.isVisible(), false);
+
+		sl.checkComplete("complete");
+		assertEquals(list.get(0).isVisible(), true);
+		assertEquals(list.get(1).isVisible(), false);
 		
 	}
 
 	public void testCheckIncomplete() {
-		ArrayList<Task> list = new ArrayList<Task>();
-		Task t1 = new Task();
-		Task t2 = new Task();
-		t1.setComplete(true);
-		t2.setComplete(false);
-		list.add(t1);
-		list.add(t2);
-		assertEquals(t1.isVisible(), false);
-		assertEquals(t2.isVisible(), true);
+		sl.checkIncomplete("incomplete");
+		assertEquals(list.get(0).isVisible(), false);
+		assertEquals(list.get(1).isVisible(), true);
 	}
 
 	public void testFilterByCat() {
-		ArrayList<Task> list = new ArrayList<Task>();
-		Task t1 = new Task();
-		Task t2 = new Task();
-		t1.setCategory("Work");
-		t2.setCategory("School");
-		list.add(t1);
-		list.add(t2);
-		assertEquals(t1.isVisible(), false);
-		assertEquals(t2.isVisible(), true);
+		sl.filterByCat("Work");
+		assertEquals(list.get(0).isVisible(), true);
+		assertEquals(list.get(1).isVisible(), false);
 	}
 
 	public void testRepopulate() {
-		ArrayList<Task> list = new ArrayList<Task>();
-		Task t1 = new Task();
-		Task t2 = new Task();
-		t1.setCategory("Work");
-		t2.setCategory("School");
-		list.add(t1);
-		list.add(t2);
+		list.get(0).setVisible(false);
+		list.get(1).setVisible(true);
+		sl.repopulate();
+		assertEquals(list.get(0).isVisible(), true);
+		assertEquals(list.get(1).isVisible(), true);
 	}
 	
 }
