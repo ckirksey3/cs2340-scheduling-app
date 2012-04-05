fced2d0febe745260bed5c19f8ce65969b415432
jpansini3 <jpansini3@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-04-05 02:36:20 +0000

    filtering works

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/SpinnerListener.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/SpinnerListener.java
@@ -1,10 +1,7 @@
 package cs2340.LetMeCheckMyApp;
-import java.util.ArrayList;
-import java.util.Calendar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Spinner;
 
 /**
  * Used to handle events raised by the filter Spinner
@@ -27,33 +24,29 @@ public class SpinnerListener implements OnItemSelectedListener {
 	 */
 	public void onItemSelected(AdapterView<?> parent,View view, int pos, long id) {
 		//repopulate the list
-		for (int i = t.getFilteredList().size()-1; i>=0; i--){
-			t.getList().add(t.getFilteredList().remove(i));
+		for (int i = 0; i< t.getList().size(); i++){
+			t.getList().get(i).setVisible(true);
 			t.getListAdapter().notifyDataSetChanged();
 		}
 		
-		//filter the list
 		String s = (String) parent.getSelectedItem(); 
-		int i = t.getList().size()-1;
-		
 		
 		if (s.equalsIgnoreCase("complete")) {
-			while (i >= 0){
+			for (int i = 0; i< t.getList().size(); i++){
 				if (t.getList().get(i).isComplete() == false){ // if not the correct category move the item to the other list for storage
-					t.getFilteredList().add(t.getList().remove(i));
+					t.getList().get(i).setVisible(false);
 					t.getListAdapter().notifyDataSetChanged();
 				}
-				i--;
 			}
 			return;
 		}
+		
 		if( s.equalsIgnoreCase("incomplete")){
-			while (i >= 0){
+			for (int i = 0; i< t.getList().size(); i++){
 				if (t.getList().get(i).isComplete() == true){ // if not the correct category move the item to the other list for storage
-					t.getFilteredList().add(t.getList().remove(i));
+					t.getList().get(i).setVisible(false);
 					t.getListAdapter().notifyDataSetChanged();
 				}
-				i--;
 			}
 			return;
 		}
@@ -62,27 +55,23 @@ public class SpinnerListener implements OnItemSelectedListener {
 			return;
 			
 		
-		while (i >= 0){
+		for (int i = 0; i< t.getList().size(); i++){
 			if (!t.getList().get(i).getCategory().equalsIgnoreCase(s)){ // if not the correct category move the item to the other list for storage
-				t.getFilteredList().add(t.getList().remove(i));
+				t.getList().get(i).setVisible(false);
 				t.getListAdapter().notifyDataSetChanged();
 			}
-			i--;
 		}
 	}
 
-	
-	
-	
 	
 	/**
 	 * Executes if none of the options are selected by the user
 	 */
 	public void onNothingSelected(AdapterView<?> parent) {
-		for (int i = t.getFilteredList().size()-1; i>=0; i--){
-			t.getList().add(t.getFilteredList().remove(i));
-			t.getListAdapter().notifyDataSetChanged();
+		for (int i = 0; i< t.getList().size(); i++){
+			t.getList().get(i).setVisible(true);
 		}
+		
 	}
 	
 	
