c3029071d4a1ae6266489611ed6f201faba224ce
ckirksey3@gmail.com <ckirksey3@gmail.com@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-04-12 16:36:22 +0000

    Added some tests for manage task list

--- trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskListTest.java
+++ trunk/LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/ManageTaskListTest.java
@@ -2,13 +2,26 @@ package cs2340.LetMeCheckMyApp;
 
 import java.util.ArrayList;
 
+import junit.framework.Assert;
+
+//import com.jayway.android.robotium.solo.Solo;
+
 import android.test.ActivityInstrumentationTestCase2;
+
+
 
 public class ManageTaskListTest extends ActivityInstrumentationTestCase2<ManageTaskList> {
 
+	private ManageTaskList mtl;
+	//private Solo solo;
 	public ManageTaskListTest() {
 		super(ManageTaskList.class);
 	}
+	
+	public void setUp() throws Exception {
+		mtl = new ManageTaskList();
+        //solo = new Solo(getInstrumentation(), getActivity());
+  }
 	
 	public void testGetAllTasks(){
 		ManageTaskList activity = this.getActivity();
@@ -22,5 +35,59 @@ public class ManageTaskListTest extends ActivityInstrumentationTestCase2<ManageT
 		boolean success = activity.deleteTask(new Task());
 		
 		assertEquals(true, success);
+	}
+	
+	 public void testAddTask() throws Exception {
+		 
+		 AddTask at = new AddTask();
+		 
+		 ArrayList<Task> taskList = mtl.getAllTasks("user");
+		 Task testTask = new Task();
+		 testTask.setName("Test Task");
+		 at.createTask(testTask);
+		 
+		 ArrayList<Task> newTaskList = mtl.getAllTasks("user");
+		 
+		 assertEquals(false, newTaskList.equals(taskList));
+		 assertEquals(false, taskList.contains(testTask));
+		 assertEquals(true, newTaskList.contains(testTask));
+		 
+		 
+		 //solo.clickOnText("Add Task");
+		 //assertTrue(solo.searchText("Description"));
+		 
+         /*solo.sendKey(Solo.MENU);
+         solo.clickOnText("More");
+         solo.clickOnText("Preferences");
+         solo.clickOnText("Edit File Extensions");
+         Assert.assertTrue(solo.searchText("rtf"));
+         
+         solo.clickOnText("txt");
+         solo.clearEditText(2);
+         solo.enterText(2, "robotium");
+         solo.clickOnButton("Save");
+         solo.goBack();
+         solo.clickOnText("Edit File Extensions");
+         Assert.assertTrue(solo.searchText("application/robotium"));*/
+         
+}
+	public void testRemoveTask() throws Exception {
+			 
+			 AddTask at = new AddTask();
+			
+			 Task testTask = new Task();
+			 testTask.setName("Test Task");
+			 at.createTask(testTask);
+			 ArrayList<Task> newTaskList = mtl.getAllTasks("user");
+
+			 assertEquals(true, newTaskList.contains(testTask));
+			 
+			 mtl.removeTask(testTask);
+			 
+			 newTaskList = mtl.getAllTasks("user");
+			 
+			 assertEquals(false, newTaskList.contains(testTask));
+			 
+	         
 	}
 }
