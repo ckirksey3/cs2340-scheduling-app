def8eb2812844b06c71f3dda9fc82d5fa4a04573
dandoozan <dandoozan@3ce0f793-afd4-3665-3ba7-8657570d8178>
2012-04-26 15:33:30 +0000

    Added functionality to remember past locations

--- LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
+++ LetMeCheckMyApp/src/cs2340/LetMeCheckMyApp/AddTask.java
@@ -6,8 +6,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
@@ -16,6 +19,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -32,6 +36,7 @@ import android.widget.Toast;
  */
 public class AddTask extends Activity {
 	private final String TASK_FILE = "tasks.dat";
+	private final String LOCATION_FILE = "locations.dat";
 	
 	/** Called when the activity is first created. */
 	
@@ -43,10 +48,16 @@ public class AddTask extends Activity {
 		final EditText taskNameET = (EditText) findViewById(R.id.TaskNameText);
 		final EditText descriptionET = (EditText) findViewById(R.id.TaskDescriptionText);
 		final DatePicker dateWidget = (DatePicker) findViewById(R.id.datePicker1);
-		final EditText locationET = (EditText) findViewById(R.id.LocationText);
-
+		//final EditText locationET = (EditText) findViewById(R.id.LocationText);
+		
+		//Populate Location
+		String[] locations = readRecentLocations();
+		final AutoCompleteTextView locationTextView = (AutoCompleteTextView) findViewById(R.id.LocationText);
+		    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, locations);
+		    locationTextView.setAdapter(adapter);
+	        
 		//Populate Category Spinner
-		 final Spinner categorySpinner = (Spinner) findViewById(R.id.TaskCategorySpinner);
+		final Spinner categorySpinner = (Spinner) findViewById(R.id.TaskCategorySpinner);
 		    ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
 		            this, R.array.category_array, android.R.layout.simple_spinner_item);
 		    spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -64,7 +75,8 @@ public class AddTask extends Activity {
 				String taskName = taskNameET.getText().toString();
 				String description = descriptionET.getText().toString();
 				String category = categorySpinner.getSelectedItem().toString();
-				String location = locationET.getText().toString();
+				//String location = locationET.getText().toString();
+				String location = locationTextView.getText().toString();
 				
 				Calendar date = Calendar.getInstance();
 				date.set(Calendar.YEAR, dateWidget.getYear());
@@ -84,6 +96,7 @@ public class AddTask extends Activity {
 				if(!taskExists(task)){ //check if task already exists
 					if(createTask(task))
 					{
+						storeLocation(location);
 						setResult(RESULT_OK, new Intent().putExtra("AddTask", task));
 						displayMessage("Task added successfully");
 						finish();
@@ -129,6 +142,25 @@ public class AddTask extends Activity {
 		
 	}
 	
+	/**
+	 * Stores a location
+	 * @param loc Location to store
+	 * @return true if the location was stored successfully, false if a problem occurred
+	 */
+	boolean storeLocation(String loc)
+	{
+		Log.d("AddTask", "Storing "+loc+" in "+LOCATION_FILE);
+		try{
+			FileOutputStream fos = openFileOutput(LOCATION_FILE, Context.MODE_APPEND);
+			fos.write((loc+"\n").getBytes());
+			fos.close();
+			return true;
+		}catch(IOException e){
+			Log.e("AddTask", e.getMessage());
+			return false;
+		}
+		
+	}
 	
 	/**
 	 * Checks if a task already exists in the database
@@ -159,6 +191,41 @@ public class AddTask extends Activity {
 			}
 		}
 		return found;
+	}
+	
+	/**
+	 * Reads locations from database
+	 * @return	array of locations
+	 */
+	String[] readRecentLocations(){
+		List<String> list = new ArrayList<String>();
+		Log.d("AddTask", "Reading in recent locations from file.");
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(getFilesDir()+"/"+LOCATION_FILE))));
+			String line;
+			while((line = br.readLine()) != null){
+				list.add(line.trim());
+			}
+		}catch(IOException e){
+			Log.e("AddTask", e.getMessage());
+		}finally {
+			try{
+				if(br != null)
+					br.close();
+			}catch(IOException e){
+				Log.e("AddTask", e.getMessage());
+			}
+		}
+		String[] array = new String[list.size()];
+		Iterator<String> it = list.iterator();
+		int cnt=0;
+		while(it.hasNext()){
+			array[cnt] = it.next();
+			cnt++;
+		}
+		return array;
+		
 	}
 	
 	/**
